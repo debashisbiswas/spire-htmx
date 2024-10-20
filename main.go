876e5d102d3f50add0aa739d602677e6df0d3a7f
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2024-10-19 19:29:21 -0700

    render entries on load

--- main.go
+++ main.go
@@ -5,13 +5,24 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
+
+type Entry struct {
+	Time    time.Time
+	Content string
+}
+
+var entries = []Entry{
+	{time.Now(), "welcome to the playground"},
+	{time.Now(), "follow me"},
+}
 
 var templates = template.Must(template.ParseFiles("templates/index.html"))
 
 func baseHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.URL.Path)
-	err := templates.ExecuteTemplate(w, "index.html", nil)
+	err := templates.ExecuteTemplate(w, "index.html", entries)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
